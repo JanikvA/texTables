cb79ded7e3bc33c87bdcb42993adb4600bc18b3d
JanikvA <j.v.ahnen@gmx.de>
2019-02-15 18:41:52 +0100

    minor additions

--- texTables.py
+++ texTables.py
@@ -3,8 +3,6 @@
 
 import numpy as np
 
-# TODO allow to set labels for colms or rows
-# TODO implement support for popular moduls like panda
 # TODO allow to instantly create pdf
 
 
@@ -13,60 +11,77 @@ class texTable(object):
 
     def __init__(self, npMat=None, arrayArray=None):
         if not (npMat is None):
-            self.mat=npMat
+            self.mat = npMat
 
         if not (arrayArray is None):
-            self.mat=np.array(arrayArray)
+            self.mat = np.array(arrayArray)
 
-        #NOTE mxn matrix: m rows
-        self.rows=len(self.mat)
-        self.colms=len(self.mat[0])
+        # NOTE mxn matrix: m rows
+        self.rows = len(self.mat)
+        self.colms = len(self.mat[0])
+
+        self.rowNames = None
+        self.colmNames = None
 
     def printTable(self):
-        cString=("c|"*self.colms)[:-1]
-        tmp=1
-        tableString='''\\begin{{table}}
-   \\centering
-   \\begin{{tabular}}{{{cs}}}
+        cString = ("c|"*self.colms)[:-1]
+        if self.rowNames:
+            cString += "|c"
+        tmp = 1
+        tableString = '''\\begin{{table}}
+\\centering
+\\begin{{tabular}}{{{cs}}}
 '''.format(cs=cString)
-        for e in np.nditer(self.mat):
-            tableString+=str(e) + " & "
-            if tmp==self.colms:
-                tmp=1
-                tableString=tableString[:-2]+"\\\\\n"
+        if self.colmNames:
+            for i, n in enumerate(self.colmNames):
+                tableString += n + " & "
+                if i == self.colms:
+                    tableString = tableString[:-2]+"\\\\\n"
+                    tableString += "\\hline\n"
+        rowNameIndex = 0
+        for i, e in enumerate(np.nditer(self.mat)):
+
+            if self.rowNames and tmp == 1:
+                tableString += self.rowNames[rowNameIndex] + " & "
+                rowNameIndex += 1
+            tableString += str(e) + " & "
+            if tmp == self.colms:
+                tmp = 1
+                tableString = tableString[:-2]+"\\\\\n"
             else:
-                tmp+=1
-        tableString+='''   \\end{tabular}
+                tmp += 1
+        tableString += '''\\end{tabular}
 \\end{table}
 '''
         print tableString
-
 
     def printStandalone(self):
         pass
 
     def setNames(self, rowNames=None, colmNames=None):
-        pass
+        self.rowNames = rowNames
+        self.colmNames = colmNames
 
     def isValid(self):
         pass
 
-
     def __str__(self):
-        print "self.mat:\n",self.mat
+        print "self.mat:\n", self.mat
+        print "self.rowNames, self.colmNames:", self.rowNames, self.colmNames
         return "texTable"
 
 
 def test():
-    testA=[[1,2,3],[4,5,6],[7,8,9]]
-    tableObj=texTable(arrayArray=testA)
+    testA = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    tableObj = texTable(arrayArray=testA)
+    colmLabels = [" ", "a", "b", "c"]
+    rowLabels = ["d", "e", "f"]
+    tableObj.setNames(rowLabels, colmLabels)
     print tableObj
     tableObj.printTable()
 
-
-
-    ta=np.array([[1, 2], [3, 4]])
-    tableObj2=texTable(ta)
+    ta = np.array([[1, 2], [3, 4]])
+    tableObj2 = texTable(ta)
 
 
 if __name__ == "__main__":
