f9366a8db445c0f9765a4e94c92adbd8fad94156
JanikvA <j.v.ahnen@gmx.de>
2019-02-16 16:05:28 +0100

    added mirror() and made labeling colm/rows easier

--- texTables.py
+++ texTables.py
@@ -5,7 +5,6 @@ import numpy as np
 
 # TODO allow to instantly create pdf
 
-
 class texTable(object):
     """This class is supposed to make the creating of tables simple"""
 
@@ -38,12 +37,11 @@ class texTable(object):
                 if i == self.colms:
                     tableString = tableString[:-2]+"\\\\\n"
                     tableString += "\\hline\n"
-        rowNameIndex = 0
+        rowNameIndex=0
         for i, e in enumerate(np.nditer(self.mat)):
-
             if self.rowNames and tmp == 1:
                 tableString += self.rowNames[rowNameIndex] + " & "
-                rowNameIndex += 1
+                rowNameIndex+=1
             tableString += str(e) + " & "
             if tmp == self.colms:
                 tmp = 1
@@ -56,11 +54,40 @@ class texTable(object):
         print tableString
 
     def printStandalone(self):
-        pass
+        print'''
+\\documentclass{article}
+\\usepackage[left=1cm, a0paper]{geometry}
+\\usepackage[graphicx]{realboxes}
+\\usepackage{bm}
+\\begin{document}\n'''
+        self.printTable()
+        print "\\end{document}"
 
     def setNames(self, rowNames=None, colmNames=None):
+        if len(colmNames)==self.colms: # This makes top left corner as empty
+            colmNames.insert(0," ")
+        elif not(len(colmNames)==self.colms+1):
+            print "WARNING: Not all colms have names"
+        if not (len(rowNames)==self.rows):
+            print "WARNING: Not all rows have names"
         self.rowNames = rowNames
         self.colmNames = colmNames
+
+    def mirror(self):
+# Makes colms->rows and rows->colms
+        newArrArr=[]
+        for a in range(self.colms):
+            tmpArr=[]
+            for b in range(self.rows):
+                tmpArr.append(self.mat[b][a])
+            newArrArr.append(tmpArr)
+        self.mat=np.array(newArrArr)
+        self.rowNames, self.colmNames= self.colmNames, self.rowNames
+        if self.rowNames:
+            self.colmNames.insert(0,self.rowNames[0])
+            self.rowNames.pop(0)
+        self.rows, self.colms= self.colms, self.rows
+
 
     def isValid(self):
         pass
@@ -74,10 +101,15 @@ class texTable(object):
 def test():
     testA = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     tableObj = texTable(arrayArray=testA)
-    colmLabels = [" ", "a", "b", "c"]
+    colmLabels = ["a", "b", "c"]
     rowLabels = ["d", "e", "f"]
     tableObj.setNames(rowLabels, colmLabels)
+    print "\n#####\n"
     print tableObj
+    print "\n#####\n"
+    tableObj.printTable()
+    tableObj.mirror()
+    print "\n#####\n"
     tableObj.printTable()
 
     ta = np.array([[1, 2], [3, 4]])
